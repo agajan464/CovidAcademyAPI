31cdb23966899ee304393f4a309481f11f29036c
Aga <janacagnieszka@gmail.com>
2020-12-12 11:04:56 +0100

    init project

---
+++ src/test/java/sdaproject/covidacademyapi/CovidacademyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sdaproject.covidacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidacademyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
